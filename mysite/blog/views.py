49759495c2196f840ff656fcec13df3c2f76fec2
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-05-19 21:04:48 +0545

    final commit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,10 +3,16 @@ from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
-
+from taggit.models import Tag
 # Create your views here.
-def post_list(request):
+def post_list(request,tag_slug=None):
     object_list = Post.objects.all()
+    tag = None
+    
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in = [tag])    
+    
     paginator = Paginator(object_list, 3) #3posts in each pages
     page = request.GET.get('page')
     try:
@@ -15,7 +21,7 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html',{'page':page,'posts':posts})
+    return render(request, 'blog/post/list.html',{'page':page,'posts':posts, 'tag':tag})
 
 
 def post_detail(request, year, month, day, post):
